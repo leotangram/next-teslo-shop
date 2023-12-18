269f7547e87ff6031e24da54ac20b733f9c5e881
Leonardo Omaña <leotangram@gmail.com>
2023-12-18 16:38:59 -0500

    nextJS font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { titleFont } from '@/config/fonts'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       <h1>Hola!</h1>
+      <h1 className={titleFont.className}>Hola!</h1>
     </main>
   )
 }
