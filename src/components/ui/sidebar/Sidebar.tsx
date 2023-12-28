a752049866a31706d347de07b45c6dfa699c1ed0
Leonardo Omaña <leotangram@gmail.com>
2023-12-28 18:30:17 -0500

    zustand

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import {
   IoCloseOutline,
@@ -9,14 +11,32 @@ import {
   IoShirtOutline,
   IoTicketOutline
 } from 'react-icons/io5'
+import { useUIStore } from '@/store'
+import clsx from 'clsx'
 
 export const Sidebar = () => {
+  const isSideMenuOpen = useUIStore(state => state.isSideMenuOpen)
+  const closeSideMenu = useUIStore(state => state.closeSideMenu)
+
   return (
     <div>
-      <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30" />
-      <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm" />
-      <nav className="fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300">
-        <IoCloseOutline className="top-5 right-0 cursor-pointer" size={50} />
+      {isSideMenuOpen && (
+        <>
+          <div className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30" />
+          <div className="fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm" />
+        </>
+      )}
+      <nav
+        className={clsx(
+          'fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300',
+          { 'translate-x-full': !isSideMenuOpen }
+        )}
+      >
+        <IoCloseOutline
+          className="absolute top-5 right-0 cursor-pointer"
+          size={50}
+          onClick={closeSideMenu}
+        />
         <div className="relative mt-14">
           <IoSearchOutline className="absolute top-2 left-2" size={20} />
           <input
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import Link from 'next/link'
 import { titleFont } from '@/config/fonts'
 import { IoCartOutline, IoSearchOutline } from 'react-icons/io5'
+import { useUIStore } from '@/store'
 
 export const TopMenu = () => {
+  const openSideMenu = useUIStore(state => state.openSideMenu)
+
   return (
     <nav className="flex px-5 justify-between items-center w-full">
       <div>
@@ -46,7 +51,10 @@ export const TopMenu = () => {
             <IoCartOutline className="w-5 h-5" />
           </div>
         </Link>
-        <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+        <button
+          className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+          onClick={openSideMenu}
+        >
           Menú
         </button>
       </div>
